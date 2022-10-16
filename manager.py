5d671e04448a24010374ef6883ccecd1f330e622
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-10-16 20:04:15 +0300

    made proper fsm

--- manager.py
+++ manager.py
@@ -1,7 +1,17 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from generator import get_key
 
 
 class FSM(StatesGroup):
     encode_state = State()
     decode_state_message = State()
     decode_state_key = State()
+
+
+def encode(message):
+    key = get_key()
+    return message, key
+
+
+def decode(message, key):
+    return message + key
--- server.py
+++ server.py
@@ -4,9 +4,8 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
-from manager import FSM
+from manager import FSM, encode, decode
 from keyboard import kb
-
 
 API_TOKEN = config.TOKEN
 
@@ -40,12 +39,45 @@ async def encode_message(message: types.Message):
 
 @dp.message_handler(state=FSM.encode_state)
 async def send_encoded(message: types.Message, state: FSMContext):
-
-    # encoded_message, key = manager.encode(message.text)
+    encoded_message, key = encode(message.text)
 
     answer = "Ваше сообщение должно быть закодировано тут"
+    answer_key = "Ключ: "
     await state.finish()
-    await message.answer(answer, reply_markup=kb)
+    await message.answer(answer)
+    await message.answer(encoded_message, reply_markup=kb)
+    await message.answer(answer_key)
+    await message.answer(key, reply_markup=kb)
+
+
+
+@dp.message_handler(commands=['decode'], state=None)
+async def decode_message(message: types.Message):
+    answer = "Введи сообщение для декодирования"
+    await FSM.decode_state_message.set()
+    await message.answer(answer, reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(state=FSM.decode_state_message)
+async def get_key(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['message'] = message.text
+
+    answer = "Введи ключ"
+    await FSM.decode_state_key.set()
+    await message.answer(answer, reply_markup=ReplyKeyboardRemove())
+
+
+@dp.message_handler(state=FSM.decode_state_key)
+async def send_encoded(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['key'] = message.text
+        decoded_message = decode(data['message'], data['key'])
+        
+    answer = "Ваше сообщение должно быть раскодировано тут"
+    await state.finish()
+    await message.answer(answer)
+    await message.answer(decoded_message, reply_markup=kb)
 
 
 if __name__ == '__main__':
